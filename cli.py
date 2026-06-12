f7089ad180ece915861565d1eba5f74abf0b86da
miniorange <kulkarninabha4@gmail.com>
2026-06-12 13:48:39 +0530

    Add command line interface argument parsing

---
+++ cli.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--symbol", required=True)
+parser.add_argument("--side", required=True)
+parser.add_argument("--type", required=True)
+parser.add_argument("--quantity", required=True, type=float)
+parser.add_argument("--price", type=float)
+
+args = parser.parse_args()
+
+print(args)
